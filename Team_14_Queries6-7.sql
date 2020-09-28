2430aae988b036b04f8ac93b3943224967052023
Erick Martinez <emartinez4@ggc.edu>
2020-09-28 03:22:32 -0400

    Added Query 6

--- Team_14_Queries6-7.sql
+++ Team_14_Queries6-7.sql
@@ -1,9 +1,11 @@
 -- Query 6
-
-
-
-
-
+-- This query displays the purchase ID, purchase date, and store ID of all purchases made after January 12th, 2020.
+-- The Purchase Date column is formatted to be read easily by human beings.
+SELECT purchaseID AS "Purchase ID", TO_CHAR(purchaseDate, '"Purchased the" fmDdth "of" Month, YYYY') AS "Purchase Date", 
+       STORES_storeID AS "Store ID"
+FROM purchase
+WHERE purchaseDate > TO_DATE('12-01-2020', 'DD-MM-YYYY')
+ORDER BY purchaseDate;
 
 
 -- Query 7 
