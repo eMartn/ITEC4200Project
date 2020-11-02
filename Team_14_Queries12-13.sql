478591db83e7ef05c4cea0d7805b6041cd0290a6
Erick Martinez <emartinez4@ggc.edu>
2020-11-01 22:53:17 -0500

    added query 13

--- Team_14_Queries12-13.sql
+++ Team_14_Queries12-13.sql
@@ -4,7 +4,15 @@ SELECT PRODUCTCATEGORY AS "Product Category", PRODUCTDESCR AS "Product Descripti
 WHERE PRODUCTPRICE < (SELECT AVG(PRODUCTPRICE) FROM PRODUCT)
 ORDER BY PRODUCTPRICE DESC;
 
-
+-- Query 13
+-- This query pulls up all of the names of those employees who
+-- live in states where there are stores that open sometime at 9am.
+SELECT e_firstName AS "First Name", e_lastName AS "Last Name"
+FROM employee
+WHERE e_state IN (SELECT storeState
+                  FROM stores
+                  WHERE openHours LIKE '9%am%-%')
+ORDER BY e_firstName;
 
 
 
